49a67db35691cf36b1a47d736a50ebe1dc4bf426
Mikel <mikel.lopez@cmz.com>
2023-02-06 10:19:12 +0100

    Change event listener

--- src/kioskboard.js
+++ src/kioskboard.js
@@ -571,7 +571,7 @@
           // remove the keyboard: end
 
           // event for input element trigger change: begin
-          var changeEvent = new Event('change', {
+          var changeEvent = new Event('input', {
             'bubbles': true,
             'cancelable': true,
           });

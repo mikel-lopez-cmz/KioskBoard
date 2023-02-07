84e8b452754f7c86a77aaa44e5bc394db3985d6e
Mikel <mikel.lopez@cmz.com>
2023-02-07 08:47:00 +0100

    Trying

--- src/kioskboard.js
+++ src/kioskboard.js
@@ -651,11 +651,11 @@
                     // add value by index
                     theInputValArray.splice(theInputSelIndex, 0, keyValArr[keyValIndex]);
 
-                    var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
-                    nativeInputValueSetter.call(input, theInputValArray.join(''));
+                    // var nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+                    // nativeInputValueSetter.call(input, theInputValArray.join(''));
 
                     // update input value
-                    // input.value = theInputValArray.join('');
+                    input.value = theInputValArray.join('');
 
                     // set next selection index
                     if (input.type !== 'number') {
